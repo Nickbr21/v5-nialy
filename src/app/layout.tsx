2f961ee0b443c3e3010a3c0ecda029262ae702a8
Nickbr21 <nicolas.baumx@gmail.com>
2025-10-22 11:51:21 -0300

    adiciona Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -38,10 +40,24 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700;900&family=Montserrat:wght@300;400;500;600;700&display=swap" 
           rel="stylesheet" 
         />
+        {/* Google Tag Manager - Script Principal */}
+<Script id="google-tag-manager" strategy="afterInteractive">
+  {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5CZKRXQS');
+  `}
+</Script>
       </head>
       <body className="font-inter antialiased">
         {children}
       </body>
+      {/* Google Tag Manager (noscript) */}
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CZKRXQS"
+height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+{/* End Google Tag Manager (noscript) */}
     </html>
   );
 }
